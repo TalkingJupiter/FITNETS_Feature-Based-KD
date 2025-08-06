20042f64a1cd1fb80d46a392f2ce8bef8b94fcde
Batuhan Sencer <batuhan.sencer@ttu.edu>
2025-08-06 14:56:54 -0500

    Adding loss script

---
+++ distiller/hint_loss.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class HintLoss(nn.Module):
+    def __init__(self, student_channels, teacher_channels):
+        super(HintLoss, self).__init__()
+        # 1x1 convolution to project student features to match teacher's dimensions
+        self.regressor = nn.Conv2d(student_channels, teacher_channels, kernel_size=1)
+
+    def forward(self, student_feat, teacher_feat):
+        """
+        Args:
+            student_feat: studnet feature map (B, C1, H, W)
+            teacher_feat: teacher feature map (B, C2, H, W)
+        Returns:
+            MSE lass between regressed student and teacher features
+        """
+        student_proj = self.regressor(student_feat)
+        return F.mse_loss(student_proj, teacher_feat)
